65354769d66467b0b0949725080bebec40a07a56
Adeazil12 <107722476+Adeazil12@users.noreply.github.com>
2022-07-23 17:48:10 +0700

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -11,14 +11,14 @@ let buttonMessage= {
 'forwardingScore':200,
 'isForwarded':true,
 'externalAdReply':{
-'mediaUrl': 'http://github.com/Warikrr/rull',
+'mediaUrl': '08980330708',
 'mediaType': 2,
 'previewType': 'pdf',
 'title': 'Mau Sc Yang Lain?,Cek Aja Channel Yt Nya',
 'body': me,
 'thumbnail': thumb2,
 'sourceUrl': 'https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg'}},
-'caption': 'http://github.com/Warikrr/rull',
+'caption': '08980330708',
 'footer': me,
 'buttons':[
 {'buttonId': _p + 'menu','buttonText':{'displayText':'ᴍᴇɴᴜ'},'type':1},
